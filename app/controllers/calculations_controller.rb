1df463c7ec7833fa2d2c2d3d5d509abcc3b0d8fb
igross22 <ian.j.gross@gmail.com>
2015-07-24 17:18:30 -0500

    Update as of 5pm, 7/24
    Killin' it

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -2,4 +2,30 @@ class CalculationsController < ApplicationController
   def instructions
     render("instructions.html.erb")
   end
+
+  def square_root
+
+    @user_input = params["number"].to_i
+
+    @square_root = Math.sqrt(@user_input)
+
+    #@user_input = Math.sqrt("number").to_i
+
+    render("square_root.html.erb")
+
+  end
+
+  def square
+
+    @user_input = params["number"].to_i
+
+    @squared = @user_input**2
+
+    #@user_input = Math.sqrt("number").to_i
+
+    render("square.html.erb")
+
+  end
+
 end
+
